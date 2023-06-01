6b76a4103d604b6ecd8c6c529311feb4b60153f7
baidyprod <dima220920041@gmail.com>
2023-06-01 15:44:17 +0300

    Performance improvements

--- backend.py
+++ backend.py
@@ -1,4 +1,3 @@
-import locale
 from datetime import datetime
 
 from dotenv import load_dotenv
@@ -6,21 +5,42 @@ from dotenv import load_dotenv
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts.prompt import PromptTemplate
 from langchain.vectorstores import Chroma
 
 load_dotenv()
 
-locale.setlocale(locale.LC_TIME, "uk_UA")
+
+def get_current_weekday_ukrainian():
+    dates_translation = {
+        "Monday": "понеділок",
+        "Tuesday": "вівторок",
+        "Wednesday": "середа",
+        "Thursday": "четвер",
+        "Friday": "п'ятниця",
+        "Saturday": "субота",
+        "Sunday": "неділя",
+    }
+    english_date = datetime.now().strftime("%A")
+    return dates_translation[english_date]
+
+
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}""" + f"""
+Current weekday: {get_current_weekday_ukrainian()}
+""" + """Standalone question:"""  # noqa: E501
+
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
 
 def run_llm(query, chat_history):
     embeddings = OpenAIEmbeddings()
     vectordb = Chroma(persist_directory="db", embedding_function=embeddings)
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectordb.as_retriever())
-    return qa(
-        {
-            "question": query + " Якщо для твоєї відповіді це потрібно, то сьогодні " + datetime.now().strftime("%A"),
-            "chat_history": chat_history,
-        }
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=vectordb.as_retriever(), condense_question_prompt=CONDENSE_QUESTION_PROMPT
     )
+    return qa({"question": query, "chat_history": chat_history})
--- readme.md
+++ readme.md
@@ -1,25 +1,36 @@
 # AI Assistant by baidy
 
-## Installation:
+## Features
+* Chat understands conversation context
+* Chat has information about current weekday
+* Data about geolocation of shops is scraped
 
+## Installation:
 * Clone the repository
 * Create and activate virtual environment
 * Create an .env file and paste OpenAI API Key like this OPENAI_API_KEY="your api key"
-* Open terminal and run
-1. pip install -r requirements.txt
-2. python init_db
-3. streamlit run main.py
+* Open terminal and run:
+1. ```bash
+   pip install -r requirements.txt
+   ```
+2. ```bash
+   python init_db
+   ```
+3. ```bash
+   streamlit run main.py
+   ```
 
 That's it! Have fun :)
 
 ## Screenshots:
-### Answering over FAQ data and shop locations data
-![picture alt](https://res.cloudinary.com/dbtmzypoa/image/upload/v1685568518/AI%20helper%20screenshots/w5qwhw6xay07yw9egmol.png "Answering over FAQ data and shop locations data")
-### Answering on dates understanding
-![picture alt](https://res.cloudinary.com/dbtmzypoa/image/upload/v1685568518/AI%20helper%20screenshots/r6ra55ozvkymnggbkd7c.png "Answering on dates understanding")
+### Geolocation questions
+![picture alt](https://res.cloudinary.com/dbtmzypoa/image/upload/v1685623033/AI%20helper%20screenshots/i9cahs568fh219qgv3qf.png)
+### FAQ questions
+![picture alt](https://res.cloudinary.com/dbtmzypoa/image/upload/v1685623033/AI%20helper%20screenshots/fqgk4weo7vpkdz9d0gwu.png)
+### Complex conversation
+![picture alt](https://res.cloudinary.com/dbtmzypoa/image/upload/v1685568518/AI%20helper%20screenshots/w5qwhw6xay07yw9egmol.png)
 
 ## Future possible improvements:
-
-* FAQ scraper, like addresses_scraper
+* FAQ scraper, like locations_scraper.py
 * Playing with prompt
 * Javascript front-end
