2753f4402438445c94322f751d0a4307e4398932
baidyprod <dima220920041@gmail.com>
2023-06-01 16:47:47 +0300

    Performance improvements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from streamlit_chat import message
 
 st.header("Технічна підтримка")
 
-prompt = st.text_input("Питання", placeholder="Введіть Ваше питання")
+query = st.text_input("Питання", placeholder="Введіть Ваше питання")
 
 if "user_prompt_history" not in st.session_state:
     st.session_state["user_prompt_history"] = []
@@ -17,15 +17,15 @@ if "chat_answers_history" not in st.session_state:
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
 
-if prompt:
+if query:
     with st.spinner("Відповідаємо.."):
-        generated_response = run_llm(query=prompt, chat_history=st.session_state["chat_history"])
+        generated_response = run_llm(query=query, chat_history=st.session_state["chat_history"])
 
         formatted_response = f"{generated_response['answer']}"
 
-        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["user_prompt_history"].append(query)
         st.session_state["chat_answers_history"].append(formatted_response)
-        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
+        st.session_state["chat_history"].append((query, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
